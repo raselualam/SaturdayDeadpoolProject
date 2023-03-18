dc2a8b5dd70428c49f13d6a15b6a0b446d8e197b
raselualam <raselualam@gmail.com>
2023-03-18 16:56:04 -0400

    test

--- src/test/java/upskill/ebay/stepDef/EbayHomePageSteps.java
+++ src/test/java/upskill/ebay/stepDef/EbayHomePageSteps.java
@@ -1,6 +1,7 @@
 package upskill.ebay.stepDef;
 
 import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import upskill.ebay.pageAction.EbayHomePageActions;
 
@@ -26,5 +27,15 @@ public class EbayHomePageSteps {
 	@Given("^Search for Big Tall Cotton Tee$")
 	public void search_for_Big_Tall_Cotton_Tee() throws Throwable {
 		EbayHomePageActionsObj.searchBigTallTee();
+	}
+	
+	@When("^Mouse Hover to MyEbay Summary$")
+	public void mouse_Hover_to_MyEbay_Summary() throws Throwable {
+
+	}
+
+	@Then("^Click on Summary$")
+	public void click_on_Summary() throws Throwable {
+
 	}
 }
